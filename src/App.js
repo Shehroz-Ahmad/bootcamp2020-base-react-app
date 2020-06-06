2512bd77c35d855fea5e0a52b81e7a1de3c6c529
shehroz ahmad <shehroz.ahmad735@gmail.com>
2020-06-06 08:26:59 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br />
+          
+          
             <b>Created By Shehroz Ahmad</b>
         </p>
         <a
